2018de124c49efd02575f27ba428c8d0c72dbb4d
Meathill <meathill@gmail.com>
2017-07-04 11:13:12 +0800

    init
    Signed-off-by: Meathill <meathill@gmail.com>

---
+++ content.md
@@ -0,0 +1,39 @@
+<!--
+title: sorting and searching
+description: A tutorial for JavaScript sorting and searching
+-->
+
+sorting and searching
+=======
+
+[@meathill](https://weibo.com/meathill/)
+
+<!-- page -->
+
+{{> author}}
+
+<!-- page -->
+
+## 教学目标
+
+1. 
+
+<!-- page -->
+
+## 课程大纲
+
+1. 
+
+<!-- page -->
+
+
+
+<!-- page -->
+
+Q&A
+
+<!-- page -->
+
+参考阅读：
+
+* 
---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const gulp = require('gulp');
+const del = require('del');
+const sequence = require('run-sequence');
+const replace = require('gulp-replace');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const stylus = require('gulp-stylus');
+const clean = require('gulp-clean-css');
+const {compiler, toCDN} = require('reveal-markdown-compiler');
+const PATH_REG = /\.\/node_modules\/([\w.\-]+)\//g;
+const DOC = 'docs/';
+
+gulp.task('clear', () => {
+  return del(`${DOC}*`);
+});
+
+gulp.task('js', () => {
+  return gulp.src('app/main.js')
+    .pipe(replace(PATH_REG, toCDN))
+    .pipe(uglify())
+    .pipe(gulp.dest(`${DOC}app/`))
+});
+
+gulp.task('stylus', () => {
+  return gulp.src('styl/screen.styl')
+    .pipe(stylus({
+      'include css': true
+    }))
+    .pipe(clean({
+      level: 2
+    }))
+    .pipe(gulp.dest(`${DOC}css/`));
+});
+
+gulp.task('copy', () => {
+  return gulp.src('img/**')
+    .pipe(gulp.dest(`${DOC}img/`));
+});
+
+gulp.task('html', () => {
+  return compiler('index.dev.html', 'content.md', `${DOC}index.html`);
+});
+
+gulp.task('default', taskDone => {
+  sequence(
+    'clear',
+    ['html', 'stylus', 'js', 'copy'],
+    taskDone
+  );
+});
